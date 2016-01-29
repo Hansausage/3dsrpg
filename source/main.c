a1aeadba65d3742973416e94767b103767c8486f
Hansausage <gbwarfi@gmail.com>
2016-01-28 21:17:19 -0500

    what the fuck is going on

--- source/main.c
+++ source/main.c
@@ -1,6 +1,34 @@
 #include <3ds.h>
 #include <stdio.h>
 
+double currFoeHP = 10;
+double chrCurrWeaponAtk = 2;
+int chrLvl = 1;
+int chrLvlAtk;
+int mgkxp, mgkdmg;
+
+int chrLvlUp() {
+    for (int i; i <= chrLvl; i++) {
+      chrLvl = chrLvl + 1;
+      chrLvlAtk = chrLvl*2+1;
+      if (mgkxp >= chrLvl) {
+          mgkdmg = chrLvl*2;
+      }
+
+
+    }
+}
+
+double attack() {
+    int chrAtk = chrLvlAtk + chrCurrWeaponAtk;
+    currFoeHP = currFoeHP - chrAtk;
+    return currFoeHP;
+}
+
+double defend() {
+    return 0;
+}
+
 
 int main() {
     //initialize both consoles
@@ -8,38 +36,8 @@ int main() {
     PrintConsole top, bottom;
     consoleInit(GFX_TOP, &top);
     consoleInit(GFX_BOTTOM, &bottom);
-    double currFoeHP = 10;
-    double chrCurrWeaponAtk = 2;
-    int chrLvl = 1;
-    int chrLvlAtk;
-    int mgkxp, mgkdmg;
-
-    int chrLvlUp() {
-        for (int i; i <= chrLvl; i++) {
-          chrLvl = chrLvl + 1;
-          chrLvlAtk = chrLvl*2+1;
-          if (mgkxp >= chrLvl) {
-              mgkdmg = chrLvl*2;
-          }
-
-
-        }
-    }
-
-    double attack() {
-        int chrAtk = chrLvlAtk + chrCurrWeaponAtk;
-        currFoeHP = currFoeHP - chrAtk;
-        return currFoeHP;
-    }
-
-    double defend() {
-        return 0;
-    }
-
 
     while (aptMainLoop()) {
-
-
 
         //scan inputs
         hidScanInput();
