d492bd7c934a6770717e866e2b71bd610f2a544f
Gabe Warfield <gbwarfi@gmail.com>
2016-02-02 14:51:08 +0000

    no idea if this works or not

--- source/main.c
+++ source/main.c
@@ -1,7 +1,15 @@
 #include <3ds.h>
 #include <stdio.h>
 
-char pointer = '>'
+//for menus and stuff
+char pointer = '>';
+//for selecting row and coloumn in \xlb[X;YH for pointer
+//example: printf("\xlb[" + x + ";" + y + "H" + pointer);
+int x, y;
+//main character's name
+char mainName = 'Goku';
+
+
 
 struct Characters {
         char name[50];
@@ -9,15 +17,19 @@ struct Characters {
         double magic[50];
 };
 
+//Player Character
 struct Characters mainChar;
 
 strcopy(mainChar.name, mainName);
 mainChar.health = 20;
 mainChar.magic = 15;
 
+//Create battle menu
 void createBMenu {
-        printf(pointer);
+        printf("\xlb[" + x + ";" + y + "H" + pointer);
         printf("\xlb[1;0HAttack");
+        printf("\xlb[1;1HDefend");
+        
 }
 
 int main() {
@@ -36,6 +48,8 @@ int main() {
         if (kDown && KEY_START) break;
         if (kDown && KEY_RIGHT) attack();
         if (kDown && KEY_LEFT) defend();
+        if (kDown && KEY_DOWN) y = y + 1;
+        if (kDown && KEY_UP && y > 0) y = y - 1;
         //flush and swap framebuffers
         gfxFlushBuffers();
         gfxSwapBuffers();
