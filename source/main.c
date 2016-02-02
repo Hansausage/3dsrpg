b5dd976e182ab6f90f11923c617f5de10d855673
Gabe Warfield <gbwarfi@gmail.com>
2016-02-02 18:45:13 +0000

    cross fingers that this works

--- source/main.c
+++ source/main.c
@@ -9,7 +9,12 @@ int x, y;
 //main character's name
 char mainName = 'Goku';
 
-
+struct Enemies {
+        char name[50];
+        int type[50];
+        double health[50];
+        double magic[50];
+};
 
 struct Characters {
         char name[50];
@@ -19,6 +24,11 @@ struct Characters {
 
 //Player Character
 struct Characters mainChar;
+struct Enemies testEnemy;
+
+strcopy(testEnemy.name, "Test Enemy");
+testEnemy.health = 10;
+testEnemy.magic = 0;
 
 strcopy(mainChar.name, mainName);
 mainChar.health = 20;
@@ -46,10 +56,10 @@ int main() {
         //check inputs
         u32 kDown = hidKeysDown();
         if (kDown && KEY_START) break;
-        if (kDown && KEY_RIGHT) attack();
-        if (kDown && KEY_LEFT) defend();
         if (kDown && KEY_DOWN) y = y + 1;
         if (kDown && KEY_UP && y > 0) y = y - 1;
+        if (y == 0 && kDown && KEY_A) attack();
+        if (y == 1 && kDown && KEY_A) defend();
         //flush and swap framebuffers
         gfxFlushBuffers();
         gfxSwapBuffers();
